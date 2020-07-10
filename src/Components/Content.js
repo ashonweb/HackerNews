ca38c2edcecdfbe66658383eab250cd424cf7155
ashonweb <meghpalbhagya@gmail.com>
2020-07-10 20:57:28 +0530

    hide functionality

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -64,144 +64,18 @@ class Content extends React.Component {
     }
     async componentDidMount() {
         var href = window.location.href;
-        console.log(href, "href")
+        // console.log(href, "href")
         console.log(href.match(/([^\/]*)\/*$/)[1], "rtyupoxcvbnl");
         let lastpath = (href.match(/([^\/]*)\/*$/)[1]);
-        console.log(('http://hn.algolia.com/api/v1/search_by_date?tags=story&numericFilters=num_comments>2&page=' + lastpath))
-
+        // console.log(('http://hn.algolia.com/api/v1/search_by_date?tags=story&numericFilters=num_comments>2&page=' + lastpath))
+        var ios = localStorage.getItem('saveddata'+lastpath) ? JSON.parse(localStorage.getItem(`saveddata`+lastpath)) : [];
+        if(ios.length !== 0){
+            this.setState({
+                search_by_date_front: ios
+            })
+        }
+        else {
         await fetch('http://hn.algolia.com/api/v1/search_by_date?tags=story&numericFilters=num_comments>2&page=' + lastpath)
-            // await fetch('http://hn.algolia.com/api/v1/search_by_date?tags=front_page&numericFilters=num_comments>2&page=' + this.state.page)
-
-
-            // fetch('http://hn.algolia.com/api/v1/search_by_date?tags=story&numericFilters=num_comments>2')
-            //  fetch('http://hn.algolia.com/api/v1/items/1',{
-
-            // })
-            .then(data => data.json())
-            .then(data => {
-                console.log(data)
-                const { hits } = data
-                // console.log(hits);
-                this.setState({
-                    search_by_date_front: hits
-                })
-            })
-            .catch((err) => {
-                console.log("Can’t access " + " response. Blocked by browser?", err);
-
-            })
-    }
-    onNextPage = () => {
-        const { page } = this.state
-        if (page === 0) {
-            this.setState({
-                page: page + 1,
-            }, () => {
-                this.props.history.push('/Next/page/' + this.state.page)
-                this.api()
-                console.log(this.state.page, "ppoopo")
-            })
-        }
-        else {
-
-            this.setState({
-                page: page + 1,
-            }, () => {
-                this.props.history.push('/Next/page/' + this.state.page)
-                this.api()
-                console.log(this.state.page, "ppoopo")
-            })
-
-            // this.setState(prevState => {
-            //     return {
-
-            //       page: page + 1
-            //     }
-            //   },()=>{
-            //     this.api()
-
-            //       console.log(this.state.page,"else")
-            //       this.props.history.push('/Next/page/' + this.state.page)
-
-            //   })
-        }
-        //     if(this.state.page === 0){
-
-        //              this.api()
-        //             this.setState({
-        //                 page:page+1,
-        //             },()=>{
-        //                 console.log(this.state.page,"if")
-
-        //             })
-
-        //     }
-        //     else{
-
-        //         this.setState(prevState => {
-        //             return {
-        //               page: prevState.page + 1
-        //             }
-        //           },()=>{
-        //             this.api()
-
-        //               console.log(this.state.page,"else")
-        //           })
-        // }
-    }
-    onPrevPage = () => {
-        const { page } = this.state
-
-        console.log(this.state.page, "drtyuikjhgf")
-        if (page === 0) {
-            alert("no going further")
-            this.props.history.push('/')
-        }
-        else {
-            this.setState(prevState => {
-                return {
-                    page: prevState.page - 1
-                }
-            }, () => {
-                console.log(this.state.page, "prev")
-                this.props.history.push('/Previous/page/' + this.state.page)
-
-                this.api();
-
-
-
-            })
-        }
-        // if(this.state.page !== 0){
-        //     this.setState({
-        //         page:page-1,
-        //     },()=>{
-        //         console.log(this.state.page,"prev")
-        //         this.api();
-        //     })
-
-
-
-        // else{
-        //     this.setState({
-        //         page:page+1,
-        //     })
-        //     console.log(this.state.page,"else condition")
-        //     this.api();
-
-        // }
-    }
-    api = () => {
-        var href = window.location.href;
-        console.log(href, "href")
-        console.log(href.match(/([^\/]*)\/*$/)[1], "rtyupoxcvbnl");
-        let lastpath = (href.match(/([^\/]*)\/*$/)[1]);
-        fetch('http://hn.algolia.com/api/v1/search_by_date?tags=story&numericFilters=num_comments>2&page=' + lastpath)
-
-            // fetch('http://hn.algolia.com/api/v1/search_by_date?tags=story&numericFilters=num_comments>2')
-            //  fetch('http://hn.algolia.com/api/v1/items/1',{
-
-            // })
             .then(data => data.json())
             .then(data => {
                 console.log(data)
@@ -215,6 +89,82 @@ class Content extends React.Component {
                 console.log("Can’t access " + " response. Blocked by browser?", err);
 
             })
+             localStorage.setItem('saveddata'+lastpath,JSON.stringify(
+                this.state.search_by_date_front
+            ))
+        }
+    }
+    onNextPage = () => {
+        const { page } = this.state
+        if (page === 0) {
+            this.setState({
+                page: page + 1,
+            }, () => {
+                this.props.history.push('/Next/page/' + this.state.page)
+                this.api()
+                // console.log(this.state.page, "ppoopo")
+            })
+        }
+        else {
+
+            this.setState({
+                page: page + 1,
+            }, () => {
+                this.props.history.push('/Next/page/' + this.state.page)
+                this.api()
+                // console.log(this.state.page, "ppoopo")
+            })
+        }
+    }
+    onPrevPage = () => {
+        const { page } = this.state
+        if (page === 0) {
+            alert("no going further")
+            this.props.history.push('/')
+        }
+        else {
+            this.setState(prevState => {
+                return {
+                    page: prevState.page - 1
+                }
+            }, () => {
+                // console.log(this.state.page, "prev")
+                this.props.history.push('/Previous/page/' + this.state.page)
+                this.api();
+            })
+        }
+            }
+    api = () => {
+        var href = window.location.href;
+        // console.log(href, "href")
+        // console.log(href.match(/([^\/]*)\/*$/)[1], "rtyupoxcvbnl");
+        let lastpath = (href.match(/([^\/]*)\/*$/)[1]);    
+        var savedata_lastpath = localStorage.getItem('saveddata'+lastpath) ? JSON.parse(localStorage.getItem(`saveddata`+lastpath)) : [];
+        if(savedata_lastpath.length !== 0){
+            this.setState({
+                search_by_date_front: savedata_lastpath
+            })
+        }   
+        else {
+        fetch('http://hn.algolia.com/api/v1/search_by_date?tags=story&numericFilters=num_comments>2&page=' + lastpath)
+            .then(data => data.json())
+            .then(data => {
+                // console.log(data)
+                const { hits } = data
+                // console.log(hits);
+                this.setState({
+                    search_by_date_front: hits
+                })
+            })
+            .catch((err) => {
+                console.log("Can’t access " + " response. Blocked by browser?", err);
+
+            })
+            localStorage.setItem('saveddata'+lastpath,JSON.stringify(
+                this.state.search_by_date_front
+            ))
+        }
+             
     }
 
     
@@ -247,15 +197,40 @@ class Content extends React.Component {
     return Math.floor(seconds) + " seconds";
   }
   
+  hideshow =(objectid)=>{
+    //   console.log(objectid)\
+
+    
+
+
+    const qs = this.state.search_by_date_front.slice();
+    const currentQs = qs.filter((q) => q.objectID !== objectid);
+    // console.log(currentQs,"cureentdd")
+    // const charLeft = 2000 - commentslength;
+    // currentQs.comments = comments;
+    // currentQs.length = charLeft;
+    this.setState({
+        search_by_date_front: currentQs,
+    },()=>{
+        var href = window.location.href;
+        let lastpath = (href.match(/([^\/]*)\/*$/)[1]);    
+        localStorage.setItem('saveddata'+lastpath,JSON.stringify(
+            this.state.search_by_date_front
+        ))
+    })
+
+      
+  }
+  
 
     render() {
         const { search_by_date_story, search_by_date_front, next } = this.state
         let b = ((this.state.search_by_date_front).length !== 0);
         let a = new Date();
-        console.log(a.toISOString())
+        // console.log(a.toISOString())
         var aDay = 24*60*60*1000;
-  console.log(this.timeSince(new Date(Date.now()-aDay)));
-  console.log(this.timeSince(new Date(Date.now()-aDay*2)));
+//   console.log(this.timeSince(new Date(Date.now()-aDay)));
+//   console.log(this.timeSince(new Date(Date.now()-aDay*2)));
         return (
             <>
                 {b ? <>
@@ -270,7 +245,8 @@ class Content extends React.Component {
 
 
                         {search_by_date_front.map((value, i) => (
-
+                            <>
+                             { this.state.hide === i ? <p>{value.title}</p> : null }
                             <tr key={i}>
 
                                 <td scomponent="th" scope="row">
@@ -285,10 +261,11 @@ class Content extends React.Component {
                                     <img style={{ width: "20px", height: "20px", cursor: "pointer" }} src={arrow} />
                                 </td>
                                 <td className="title_url_author" component="th" scope="row">
-                            {value.title} <span><a style={{ color: "grey", fontSize: "10px" }} href={value.url}>{value.url}</a></span> <span style={{ color: "grey", fontSize: "10px" }}>by</span> <span style={{ color: "black", fontSize: "10px" }}>{value.author}</span> <span style= {{ color: "grey", fontSize: "10px" }}>{this.timeSince(Date.parse(value.created_at))} ago </span>
+                            {value.title} <span><a style={{ color: "grey", fontSize: "10px" }} href={value.url}>{value.url}</a></span> <span style={{ color: "grey", fontSize: "10px" }}>by</span> <span style={{ color: "black", fontSize: "10px" }}>{value.author}</span> <span style= {{ color: "grey", fontSize: "10px" }}>{this.timeSince(Date.parse(value.created_at))} ago </span> <span onClick={() => this.hideshow(value.objectID)} style={{ cursor:"pointer",color: "black", fontSize: "10px" }}>[hide]</span>
                                 </td>
 
                             </tr>
+                            </>
 
                         ))
                         }
